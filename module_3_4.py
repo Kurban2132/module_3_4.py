9722e6f10e594578bb0fc96898d4bbcefcd3bec0
Kurban2132 <k.kokanov@yandex.ru>
2024-08-22 22:37:10 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,21 @@
+# Исходный код:
+# result1 = single_root_words("rich", "richiest", "orichalcum", "cheers", "riches")
+# result2 = single_root_words("Disablement", "Able", "Mable", "Disable", "Bagel")
+# print(result1)
+# print(result2)
+def single_root_words(root_word, *other_words):
+    other_words1 = ["rich", "richiest", "orichalcum", "cheers", "riches"]
+    other_words2 = ["Disablement", "Abble", "Mable", "Disable", "Bagel"]
+    same_words = []
+    for i in range(len(other_words1)):
+        print(other_words1)
+        print(other_words1[1:5:])
+        print(other_words2[1::2])
+        
+
+
+
+
+single_root_words("")
+
+
